fdbe109f76b7bbb93f64c58175d66b3d94ba3f59
Paul Hayes <paul.hayes@entropedia.co.uk>
2017-01-21 13:06:24 +0000

    Radar arm detector (orange bit) now moves upon right trigger being pressed

--- Assets/RadioGame/Scripts/RadarGame.cs
+++ Assets/RadioGame/Scripts/RadarGame.cs
@@ -7,25 +7,38 @@ public class RadarGame : MonoBehaviour {
     public Signal[] signals;
 
     [SerializeField]
-    GameObject armPivot;
+    GameObject armPivot, detector;
+
+    [SerializeField]
+    float detectorSpeed, detectorMinY, detectorMaxY;
 
     float xAxis;
     float yAxis;
+    float zAxis;
 
     // Use this for initialization
     void Start() {
-
+        //detector = armPivot.transform.FindChild("Detector").gameObject;
     }
 
     // Update is called once per frame
     void Update() {
-        xAxis = Input.GetAxisRaw("Horizontal");
-        yAxis = Input.GetAxisRaw("Vertical");
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+        zAxis = Input.GetAxis("Z Axis");
 
-        if (Mathf.Abs(xAxis) >= 0.1f || Mathf.Abs(yAxis) >= 0.1f)
+        if (Mathf.Abs(xAxis) >= 0.2f || Mathf.Abs(yAxis) >= 0.2f)
         {
+            ShowArm();
             SetArmAngle();
+            MoveDetector();
+
+        } else
+        {
+            HideArm();
         }
+
+        Debug.Log(zAxis);
         
     }
 
@@ -33,5 +46,29 @@ public class RadarGame : MonoBehaviour {
     {
         float angle = (Mathf.Atan2(yAxis, xAxis) * Mathf.Rad2Deg) - 90f;
         armPivot.transform.localRotation = Quaternion.Euler(armPivot.transform.localRotation.x, armPivot.transform.localRotation.y, angle);
+    }
+
+    void ShowArm ()
+    {
+        armPivot.SetActive(true);
+    }
+
+    void HideArm ()
+    {
+        armPivot.SetActive(false);
+    }
+
+    void MoveDetector ()
+    {
+        Vector3 detectorPos = detector.transform.localPosition;
+        /*if (zAxis > 0 && detectorPos.y <= detectorMaxY)
+        {
+            detector.transform.localPosition = new Vector3(detectorPos.x, detectorPos.y + zAxis * detectorSpeed, detectorPos.z);
+        } else if (zAxis < 0 && detectorPos.y >= detectorMinY)
+        {
+            detector.transform.Translate(armPivot.transform.up * zAxis * detectorSpeed * Time.deltaTime);
+        }*/
+        if (zAxis >= 0)
+            detector.transform.localPosition = new Vector3(detectorPos.x, detectorMinY + (zAxis * detectorMaxY * 2), detectorPos.z);
     }
 }
