6960b69279e672243163b058f43a26a74d4352d5
Paul Hayes <paul.hayes@entropedia.co.uk>
2017-01-21 19:57:49 +0000

    Debris field spawning

---
+++ Assets/SpaceGame/Scripts/DebrisPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebrisPool : MonoBehaviour {
+
+    public PlayerShip ship;
+    public BoxCollider spawnBox;
+    public GameObject[] debrisTemplates;
+    public int numDebris;
+    public float outerSpawnRing;
+
+    GameObject[] spawnedDebris;
+
+
+	void Start () {
+        spawnedDebris = new GameObject[numDebris];
+        Bounds box = spawnBox.bounds;
+        spawnBox.transform.position = ship.transform.position;
+
+        for (int i = 0; i < numDebris; i++) {
+            Vector3 pos = new Vector3(Random.Range(-box.extents.x, box.extents.x), Random.Range(-box.extents.y, box.extents.y), Random.Range(-box.extents.z, box.extents.z));
+            spawnedDebris[i] = Instantiate(debrisTemplates[Random.Range(0, debrisTemplates.Length)], spawnBox.transform.position + pos, Quaternion.identity) ;
+            spawnedDebris[i].GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere;
+        }
+
+	}
+	
+	void Update () {
+        spawnBox.transform.position = ship.transform.position;
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.LogFormat("Exitied {0}", other);
+        other.attachedRigidbody.velocity = Vector3.zero;
+
+        Bounds box = spawnBox.bounds;
+        float xSign = Mathf.Sign(ship.body.velocity.x);
+        float zSign = Mathf.Sign(ship.body.velocity.z);
+        bool xBigger = Mathf.Abs(ship.body.velocity.normalized.x) > Random.value ;
+
+        Vector3 pos = new Vector3(xSign * Random.Range(!xBigger ? -box.extents.x : outerSpawnRing* box.extents.x, box.extents.x), Random.Range(-box.extents.y, box.extents.y), zSign*Random.Range(xBigger?-box.extents.z: outerSpawnRing * box.extents.z, box.extents.z));
+        /*
+        if (!xBigger && Mathf.Abs(pos.x) < (outerSpawnRing*box.extents.x)) {
+            pos.x = Mathf.Sign(pos.x) * outerSpawnRing *  box.extents.x;
+        }
+        if (xBigger && Mathf.Abs(pos.z) < (outerSpawnRing * box.extents.z))
+        {
+            pos.z = Mathf.Sign(pos.z) * outerSpawnRing * box.extents.z;
+        }
+        */
+        other.transform.position = pos + spawnBox.transform.position;
+    }
+}
--- Assets/SpaceGame/Scripts/PlayerShip.cs
+++ Assets/SpaceGame/Scripts/PlayerShip.cs
@@ -10,6 +10,7 @@ public class PlayerShip : MonoBehaviour {
     public Transform ship;
     public BlurOptimized blurFX;
     public ParticleSystem thrustParticles;
+    public ParticleSystem[] steeringThruster;
     public Rigidbody body;
 
     public bool mapMode;
@@ -18,6 +19,7 @@ public class PlayerShip : MonoBehaviour {
     public float zoomSmooth;
     public float zoomMasSpeed;
     public float thrust;
+    public float thrustRotation;
 
     float zoomSpeed;
 
@@ -25,28 +27,60 @@ public class PlayerShip : MonoBehaviour {
 
     public float cameraEase;
     ParticleSystem.EmissionModule emissions;
+    ParticleSystem.EmissionModule[] steeringEmitters;
 
 	void Start () {
         emissions = thrustParticles.emission;
+        steeringEmitters = new ParticleSystem.EmissionModule[steeringThruster.Length];
+
+        for (int i = 0; i < steeringThruster.Length; i++) {
+            steeringEmitters[i] = steeringThruster[i].emission;
+        }
 	}
 	
 	void Update () {
-        if (Input.GetButtonDown("Fire2")) {
+        if (Input.GetButtonDown("Fire4")) {
             mapMode = !mapMode;
         }
 
         emissions.enabled = Input.GetAxis("Z Axis")>0;
-        
 
-        CameraPos();
 
         position = body.position;
 
     }
 
+    protected void LateUpdate()
+    {
+        //transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+    }
+
     private void FixedUpdate()
     {
-        body.AddForce(body.transform.forward * Mathf.Clamp01(Input.GetAxis("Z Axis")) );
+        body.AddForce(thrust * body.transform.forward * Mathf.Clamp01(Input.GetAxis("Z Axis")) );
+        //body.AddTorque(thrustRotation * Input.GetAxis("Horizontal") * Vector3.up );
+        Vector3 rotThrust = Vector3.zero;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rotThrust =  thrustRotation * Input.GetAxis("Horizontal") * Vector3.up;
+            
+        }
+        else {
+            rotThrust = -thrustRotation * body.angularVelocity;
+        }
+
+        body.angularVelocity += rotThrust;
+
+        float thresh = 0.01f;
+        bool thrustersOn = rotThrust.magnitude > thresh;
+        bool thrusterRight = rotThrust.y > thresh;
+        bool thrusterLeft = rotThrust.y < -thresh;
+
+        steeringEmitters[0].enabled = steeringEmitters[2].enabled = thrustersOn && thrusterLeft;
+        steeringEmitters[1].enabled = steeringEmitters[3].enabled = thrustersOn && thrusterRight;
+
+        //body
+        CameraPos();
 
     }
 
@@ -54,8 +88,9 @@ public class PlayerShip : MonoBehaviour {
         Vector3 pos = playerCameras.position;
         pos.y = Mathf.SmoothDamp(pos.y, mapMode ? zoomOutPos : zoomInPos, ref zoomSpeed, zoomSmooth, zoomMasSpeed);
 
-        blurFX.enabled = Mathf.InverseLerp(zoomInPos, zoomOutPos, pos.y) < 0.5f;
-        mainCamera.enabled = Mathf.InverseLerp(zoomInPos, zoomOutPos, pos.y) < 0.5f;
+        float threshold = 0.1f;
+        blurFX.enabled = Mathf.InverseLerp(zoomInPos, zoomOutPos, pos.y) < threshold;
+        mainCamera.enabled = Mathf.InverseLerp(zoomInPos, zoomOutPos, pos.y) < threshold;
 
         pos.x += Time.deltaTime * cameraEase * (ship.position.x - pos.x);
         pos.z += Time.deltaTime * cameraEase * (ship.position.z - pos.z);
