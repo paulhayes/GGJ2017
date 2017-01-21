0432d9f2415539d8965cec914cb234ef82b6ea38
Paul Hayes <paul.hayes@entropedia.co.uk>
2017-01-21 11:55:19 +0000

    Created prototype radar objects

---
+++ Assets/RadioGame/Scripts/Signal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Signal {
+
+    Transform position;
+    float outerRange, innerRange;
+    AudioClip clip;
+    float time;
+}
